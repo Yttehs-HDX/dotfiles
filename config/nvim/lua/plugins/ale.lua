fc10d7f9f0558ffb8407d940e8f7d16824f4e868
Yttehs <shetty.yttehs@outlook.com>
2025-03-22 22:33:56 +0800

    feat: [neovim] add more ale entry

--- config/nvim/lua/plugins/ale.lua
+++ config/nvim/lua/plugins/ale.lua
@@ -1,21 +1,27 @@
 return {
 	'dense-analysis/ale',
 	config = function()
-		-- Configuration goes here.
 		local g = vim.g
 		g.ale_ruby_rubocop_auto_correct_all = 1
 		g.ale_linters = {
-			ruby = {'rubocop', 'ruby'},
-			lua = {'lua-format', 'luafmt', 'stylua'},
-			python = {'flake8'},
-			java = {'eclim'},
-			rust = {'rustfmt', 'cargo'},
-			go = {'golangci-lint'},
-			kotlin = {'ktlint'},
-			bash = {'shellcheck'},
-			sh = {'shellcheck'},
-			json = {'jsonlint'},
-			toml = {'tomllint'}
+			ruby = { 'rubocop', 'ruby' },
+      lua = { 'lua-format', 'luafmt', 'stylua' },
+      python = { 'flake8', 'pylint', 'mypy' },
+      java = { 'eclim' },
+      rust = { 'rustfmt', 'cargo' },
+      go = { 'golangci-lint' },
+      kotlin = { 'ktlint' },
+      bash = { 'shellcheck' },
+      sh = { 'shellcheck' },
+      json = { 'jsonlint' },
+      toml = { 'tomllint' },
+      javascript = { 'eslint', 'jshint' },
+      typescript = { 'eslint' },
+      html = { 'tidy' },
+      css = { 'stylelint' },
+      c = { 'clang-tidy' },
+      cpp = { 'clang-tidy' },
+      dockerfile = { 'hadolint' },
 		}
 	end
 }
